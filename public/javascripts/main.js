ba9895de5e321b5e05b1468b3ed68d554b8c65d8
Coleman <charleebrown22@gmail.com>
2023-07-17 09:52:17 -0500

    Changed to five cards.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,11 @@
 var x = 3;
 var mainID = "";
+
+
 newDeck().then(val =>  {
 	mainID = val}
 	)
+
 function newDeck() {
     return fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
         .then(res => res.json())
@@ -13,11 +16,12 @@ function newDeck() {
 }
 
 function getACard(deck) {
-    return fetch('https://deckofcardsapi.com/api/deck/' + deck + '/draw/?count=1')
+    return fetch('https://deckofcardsapi.com/api/deck/' + deck + '/draw/?count=5')
         .then(res => res.json())
         .then(json => {
             const data = json;
-            return [data.cards[0].value, data.cards[0].suit, data.cards[0].image]
+            console.log(data);
+            return data;
         });
 }
 function pickAcard(){
@@ -25,12 +29,17 @@ function pickAcard(){
         var cards = document.getElementById("cardValue");
         getACard(mainID)
             .then(cardValue => {
+                var tests = document.getElementsByClassName("cardSet");
+                var names = document.getElementsByClassName("cardInfo")
 				var imgID = document.getElementById("picture");
-				imgID.src = cardValue[2];
-                cards.innerText = cardValue[0] + " of " + cardValue[1];
+                for(var i=0; i<tests.length; i++) {
+                names[i].innerText = cardValue.cards[i].value + " of " + cardValue.cards[i].suit;
+                tests[i].src = cardValue.cards[i].image;
+                }
+				//imgID.src = cardValue[2];
+                //cards.innerText = cardValue[0] + " of " + cardValue[1];
 				console.log(cardValue[1]);
             });
-    
 }
 
 function PullNewDeck(){
