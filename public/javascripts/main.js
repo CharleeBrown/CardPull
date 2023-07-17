213e59774af56172466761ee40eb5ce0c09ba875
Coleman <charleebrown22@gmail.com>
2023-07-17 13:51:54 -0500

    Cards shown matches available cards

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,11 @@
 var x = 3;
 var mainID = "";
+var totalVal = 0;
 
-
-newDeck().then(val =>  {
-	mainID = val}
-	)
+newDeck().then(val => {
+    mainID = val
+}
+)
 
 function newDeck() {
     return fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
@@ -24,24 +25,48 @@ function getACard(deck) {
             return data;
         });
 }
-function pickAcard(){
+function pickAcard() {
+    var cards = document.getElementById("cardValue");
+    getACard(mainID).then(cardValue => {
+        var tests = document.getElementsByClassName("cardSet");
+        var names = document.getElementsByClassName("cardInfo");
+        var setVal = document.getElementById("mainVal");
 
-        var cards = document.getElementById("cardValue");
-        getACard(mainID)
-            .then(cardValue => {
-                var tests = document.getElementsByClassName("cardSet");
-                var names = document.getElementsByClassName("cardInfo")
-				var imgID = document.getElementById("picture");
-                for(var i=0; i<tests.length; i++) {
-                names[i].innerText = cardValue.cards[i].value + " of " + cardValue.cards[i].suit;
-                tests[i].src = cardValue.cards[i].image;
-                }
-				//imgID.src = cardValue[2];
-                //cards.innerText = cardValue[0] + " of " + cardValue[1];
-				console.log(cardValue[1]);
-            });
+        let totalVal = 0;
+        
+        const valueMap = {
+            "JACK": 10,
+            "KING": 10,
+            "QUEEN": 10,
+            "ACE": 1,
+        };
+
+        // Clear existing images and text content
+        for (let i = 0; i < tests.length; i++) {
+            tests[i].style.visibility = "hidden";
+            tests[i].removeAttribute("src");
+            names[i].textContent = "";
+        }
+
+        // Populate with new card values
+        for (let i = 0; i < cardValue.cards.length; i++) {
+            const card = cardValue.cards[i];
+            names[i].textContent = `${card.value} of ${card.suit}`;
+            tests[i].src = card.image;
+            tests[i].style.visibility = "visible";
+            totalVal += valueMap[card.value] || parseInt(card.value);
+        }
+
+        console.log(totalVal);
+        setVal.textContent = totalVal;
+        //imgID.src = cardValue[2];
+        //cards.innerText = cardValue[0] + " of " + cardValue[1];
+        console.log(cardValue[1]);
+    });
 }
 
-function PullNewDeck(){
-	location.reload();
+
+
+function PullNewDeck() {
+    location.reload();
 }
